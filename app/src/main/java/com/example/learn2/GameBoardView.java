29fdafb5dfb822c2e250d26bf16a79b1edac4814
Mohamad Taher <tahr33288@gmail.com>
2024-02-19 02:02:21 -0700

    Commit. 5.1.0
    1: Made the code much shorter and deleted all reduntent variables and methods.
    
    2: found the solution to double tab bug that made it possible to play before the RandomPlayer finished playing.
    3: Made a Circular array that make me circle through all players that are playing in turns.

--- app/src/main/java/com/example/learn2/GameBoardView.java
+++ app/src/main/java/com/example/learn2/GameBoardView.java
@@ -253,7 +253,7 @@ public class GameBoardView extends View {
 		updateCellSizes(cellSize);
 		setMeasuredDimension(width, viewHeight);
 	}
-	
+
 	private void createCells() {
 		for (int row = 0; row < NUM_ROWS; row++) {
 			for (int col = 0; col < NUM_COLUMNS; col++) {
